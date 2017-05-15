5141b4490a5fd7531c67e8b30af4a77c8087d071
memmmsa <memmmsa@yahoo.com>
2017-05-15 10:35:29 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>إدارة المواد</title>
+        <link rel="shortcut icon" type="text/css" href="includes/images/MOE_Logo.ico">
+    </head>
+    <frameset rows="175,*,50" frameborder="0" framespacing="0">
+        <frame src="Head.php" noresize name="HeadPage"></frame>
+        <frameset cols="170,*,170">
+            <frame src="Login/index.php" noresize name="LeftPage"></frame>
+            <frame src="Main.php"        noresize name="MainPage"></frame>
+            <frame src="Links.php"       noresize></frame>
+        </frameset>
+        <frame src="Foot.php" noresize></frame>
+    </frameset>
+</html>
